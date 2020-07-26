abb840f4cd4f1593a71d60078509bda57af0b7d3
jamilOlaEnergy <68688516+jamilOlaEnergy@users.noreply.github.com>
2020-07-26 12:27:32 +0400

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.get('/', function (req, res) {
     });
 });*/
 
-app.get('/test', function (req, res) {
+app.get('/', function (req, res) {
     res.send("Hello World out")
 });
 app.listen(port, () => console.log("Server is running..on port " + port));
