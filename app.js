0af141bbc9db5bbb022ef07d1f8eae5ec3a3c4af
jamilOlaEnergy <68688516+jamilOlaEnergy@users.noreply.github.com>
2020-07-26 11:55:48 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
-
-
 var express = require('express');
 var app = express();
-var port=process.env.port || 3000;
+var sql = require("mssql");
+const port=process.env.PORT || 3000;
+
+
+    // config for your database
+    var config = {
+        user: 'datareader',
+        password: 'dbdatareader2020',
+        server: 'sql2019db.northeurope.cloudapp.azure.com', 
+        database: 'LTNTST' 
+    };
+
+    // connect to your database
+/*    sql.connect(config, function (err) {
+    
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+           
+        // query to the database and get the records
+        request.query('SELECT top (2) IDCUST,NAMECUST FROM LTNTST.dbo.ARCUS', function (err, recordset) {
             
 app.get('/', function (req, res) {
-    res.send("Hello World!!")
-});
+   
+    
+            if (err) console.log(err)
 
+            // send records as a response
+            res.send(recordset);
+            
+        });
+    });
+});
+*/
 app.listen(port, () => console.log("Server is running..on port " + port));
